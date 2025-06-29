04059b9cbb0ed852a0328212a56a36c29c3b13e0
MetaIllusion <xkjing@outlook.com>
2025-06-29 10:11:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,7 @@ export const siteConfig: SiteConfig = {
   themeColor: {
     hue: 50,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true,     // Hide the theme color picker for visitors
+    darkMode: 'dark'
   },
   banner: {
     enable: true,
